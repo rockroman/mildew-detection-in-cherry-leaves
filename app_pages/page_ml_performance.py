a99a42b3189a921a788c0ccaaa5c63a02cb60f9b
Fercha Pombo <ferchapombo@gmail.com>
2024-03-04 17:43:33 +0000

    Code page_ML_performance

--- app_pages/page_ml_performance.py
+++ app_pages/page_ml_performance.py
@@ -11,13 +11,25 @@ def page_ml_performance_metrics():
 
     st.write('### Train, Validation and Test Set: Labels Frequencies')
 
-
+    labels_distribution = plt.imread(f'outputs/{version}/labels_distribution.png')
+    st.image(labels_distribution, caption='Model Training Acurracy')
+    st.write("---")
 
 
     st.write('### Model History')
+    col1, col2 = st.beta_columns(2)
+    with col1:
+        model_acc = plt.imread('outputs/{version}/model_training_acc.png')
+        st.image(model_acc, caption='Model Training Accuracy')
+
+    with col2:
+        model_loss = plt.imread('outputs/{version}/model_training_losses.png')# this one is still not rendered 
+        st.image(model_loss, caption="Model Training Losses")
+        st.write('---')
 
 
 
 
     st.write('### Generalized Performamce on Test Set')
+    st.dataframe(pd.DataFrame(load_test_evaluation(version), index=['Loss', 'Accuracy']))
     
